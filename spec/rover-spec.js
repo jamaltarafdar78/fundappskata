7e00954a385b3e65704d90e7e2a28e05a49202a1
Jamal H. Tarafdar <jamal.tarafdar@gmail.com>
2018-02-18 12:03:00 +0000

    add initial test - rover moves nowhere

--- spec/rover-spec.js
+++ spec/rover-spec.js
@@ -1,5 +1,20 @@
-describe('test setup', () => {
-    it('SHOULD run these tests', () => {
-        console.info('running rover-spec');
-    });
+var rover = require('../src/rover');
+
+let roverNewState;
+
+const roverInitialState = {
+    x_pos: 0, 
+    y_pos: 0,
+    heading: 'N'
+};
+
+describe('running tests', () => {
+    describe('move rover nowhere', () => {
+
+        roverNewState = rover.move('');
+    
+        it('SHOULD remain at inital state', () => {
+            expect(roverNewState).toEqual(roverInitialState);
+        })
+    })
 });
---
+++ src/rover.js
@@ -0,0 +1,13 @@
+const move = () => ({
+    x_pos: 0, 
+    y_pos: 0,
+    heading: 'N'
+})
+
+module.exports = {
+    F: 'f',
+    B: 'b',
+    L: 'l',
+    R: 'r',
+    move: move
+}
