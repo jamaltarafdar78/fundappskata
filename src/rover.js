a8eab3c19580f04be6641d54da7faf52c562b2e2
Jamal H. Tarafdar <jamal.tarafdar@gmail.com>
2018-02-18 12:05:01 +0000

    refactor implementation - rover moves nowhere

--- src/rover.js
+++ src/rover.js
@@ -1,8 +1,12 @@
-const move = () => ({
+const initialState = {
     x_pos: 0, 
     y_pos: 0,
     heading: 'N'
-})
+}
+
+const move = (moveComm) => {
+    if(moveComm === '') return initialState;
+}
 
 module.exports = {
     F: 'f',
